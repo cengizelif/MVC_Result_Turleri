6ff72edcdd1524fa6665e728f330e5045c39a8c7
cengizelif <cenelif@gmail.com>
2023-04-11 14:08:24 +0300

    Add project files.

---
+++ MVC_Result_Turleri/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using MVC_Result_Turleri.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Result_Turleri.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public RedirectResult Index2()
+        {
+            // return RedirectToAction("Index");
+            // return Redirect("/Home/Index");
+            return Redirect("https://google.com");
+        }
+
+        public JsonResult Index3()
+        {
+            Urun u = new Urun();
+            u.Id = 1;
+            u.Adi = "bilgisayar";
+            u.Fiyati = 20000;
+            u.Aciklama = "Yeni ürün";
+            return Json(u, JsonRequestBehavior.AllowGet);
+        }
+
+        static List<string> veriler = new List<string>();
+
+        public ActionResult AnaSayfa(string ad)
+        {
+            ViewBag.liste = veriler;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AnaSayfa(string ad, string soyad)
+        {
+            veriler.Add(ad + " " + soyad);
+
+            return new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new
+            {
+                action = "AnaSayfa",
+                controller = "Home",
+                kod = Guid.NewGuid().ToString()
+            }));
+
+        }
+
+        public ActionResult Dosyalar()
+        {
+            return View();
+        }
+
+        public FileResult PDFDosyaGoster()
+        {
+            string dosya = Server.MapPath("~/Dosyalar/ch01.pdf");
+            return new FilePathResult(dosya, "application/pdf");
+        }
+
+        public FileStreamResult MetinDosyasiIndir()
+        {
+            MemoryStream memo = new MemoryStream();
+            string metin = "Bu bir deneme yazısıdır";
+            byte[] bytes = Encoding.UTF8.GetBytes(metin);
+            memo.Write(bytes, 0, bytes.Length);
+            memo.Position = 0;
+
+            FileStreamResult sonuc = new FileStreamResult(memo, "text/plain");
+            sonuc.FileDownloadName = "deneme.txt";
+
+            return sonuc;
+        }
+
+        public PartialViewResult KategoriGetir()
+        {
+            return PartialView("_KategorilerPartialPage");
+        }
+
+        public PartialViewResult KategoriGetir2()
+        {
+            List<string> kategoriler = new List<string>()
+             {
+                "Teknoloji","Giyim","Gıda","Temizlik"
+             };
+            return PartialView("_PartialPageKategoriler2", kategoriler);
+        }
+
+        public JavaScriptResult Mesaj()
+        {
+            string mesaj = "alert('Merhaba Dünya')";
+            return JavaScript(mesaj);
+        }
+
+        public JavaScriptResult ButtonMesaj()
+        {
+            string js = "function button_click() { alert('Merhaba Dünya');}";
+            return JavaScript(js);
+        }
+
+    }
+}
---
+++ MVC_Result_Turleri/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVC_Result_Turleri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVC_Result_Turleri")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9874f9b2-788b-46fb-8ab8-38824fd8990d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
